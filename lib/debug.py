fd30f95ca1e083acbc2b218535212dd1d8d3a52e
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-03-19 08:47:30 +0300

    Completed Working with Seed Data

--- lib/debug.py
+++ lib/debug.py
@@ -15,5 +15,8 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    botw = Game(title="Breath of the Wild", platform="Switch", genre="Adventure", price=60)
+    session.add(botw)
+    session.commit()
 
     import ipdb; ipdb.set_trace()
